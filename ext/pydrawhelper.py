cb5aa58c565081eb87c1a2a7f31ea2ff9b828157
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-08-07 13:16:53 +0200

    update DrawHelper source code (oh boy was it ugly)

--- ext/pydrawhelper.py
+++ ext/pydrawhelper.py
@@ -1,155 +1,137 @@
 # Copyright (C) 2011-2016  Niklas Rosenstein
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-import  c4d
-from    c4d.plugins         import ObjectData, RegisterObjectPlugin
-from    c4d.bitmaps         import BaseBitmap
-from    c4d.gui             import GeDialog
-
-from    os.path             import join, split, exists
+from __future__ import print_function
+import c4d
+import os
+import traceback
 
 __res__ = require('c4ddev/__res__')
 utils = require('c4ddev/utils')
 
-def GetMessageID(data):
-    """
-    Returns the ID the data-object carries,
-    i.e. when a button is pressed, or None
-    if no ID could be obtained.
-    """
-    if data is None:
-        return
-    try:
-        return data["id"][0].id
-    except:
-        return
-
-class DrawPassHelperEditor(GeDialog):
+class DrawPassHelperEditor(c4d.gui.GeDialog):
     DRAWHELPER_EDITOR   = 10001
     BTN_COMMIT          = 10002
     EDT_SOURCE          = 10003
 
-    def __init__(self, drawPassHelperInstance):
-        self.dphInstance    = drawPassHelperInstance
+    def __init__(self, op):
+        self.op = op
+
+    def SetDrawHelper(self, op):
+        self.op = op
+        if self.IsOpen():
+            self.SetSource(op[c4d.DRAWHELPER_SOURCE])
 
     def CreateLayout(self):
         self.LoadDialogResource(self.DRAWHELPER_EDITOR, flags = c4d.BFH_SCALEFIT | c4d.BFV_SCALEFIT)
+        return True
 
+    def InitValues(self):
+        if self.op:
+            self.SetSource(self.op[c4d.DRAWHELPER_SOURCE])
         return True
 
     def Command(self, id, msg):
         if id == self.BTN_COMMIT:
             self.Commit()
-
         return True
 
     def DestroyWindow(self):
         self.Commit()
 
     def Commit(self):
-        self.dphInstance.SendSourcecode( self.GetString(self.EDT_SOURCE) )
+        self.op[c4d.DRAWHELPER_SOURCE] = self.GetString(self.EDT_SOURCE)
         c4d.DrawViews()
 
-    def SetSourcecode(self, code):
-        self.SetString( self.EDT_SOURCE, code )
+    def SetSource(self, code):
+        self.SetString(self.EDT_SOURCE, code)
 
-class DrawPassHelper(ObjectData):
-    PLUGIN_ID           = 1027193
+class DrawPassHelper(c4d.plugins.ObjectData):
+    PLUGIN_ID = 1027193
+    PLUGIN_NAME = "c4ddev Draw Helper"
+    Editor = DrawPassHelperEditor(None)
 
-    try:
-        fl              = open( join( utils.plugin_dir, "res", "PyDrawHelper_InitialSource.py" ), "r")
-
-        INIT_SOURCECODE = ""
-        for i in fl:
-            INIT_SOURCECODE    += i
-
-
-    except IOError:
-        INIT_SOURCECODE = "import c4d\n\ndef main():\n    pass\nmain()"
-
-    finally:
-        fl.close()
-
+    @staticmethod
+    def get_initial_source():
+        filename = os.path.join(utils.plugin_dir, "res", "PyDrawHelper_InitialSource.py")
+        try:
+            with open(filename) as fp:
+                return fp.read()
+        except (OSError, IOError) as exc:
+            print(exc)
+            return "import c4d\n\ndef main(op, drawpass, bd, bh):\n    pass\n"
 
     def __init__(self):
-        self.op     = None
-        self.editor = DrawPassHelperEditor(self)
-
-    def __repr__(self):
-        return "<DrawPassHelper instance>"
+        self.op = None
+        self.func = None
+        self.code_hash = 0
 
     def Init(self, op):
-        self.op     = op
+        self.op = op
         self.InitAttr(op, str, [c4d.DRAWHELPER_SOURCE])
-
-        op[c4d.DRAWHELPER_SOURCE]   = self.INIT_SOURCECODE
-
+        op[c4d.DRAWHELPER_SOURCE] = self.get_initial_source()
         return True
 
     def Execute(self, op, doc, bt, prority, flags):
         return True
 
-    def Message(self, op, type, data):
-        if data is None: return True
-
-        id      = GetMessageID(data)
-
-        if id == c4d.DRAWHELPER_OPENEDITOR:
-            self.editor.Open( c4d.DLG_TYPE_ASYNC, self.PLUGIN_ID, 250, 200, 600, 500)
-            self.editor.SetSourcecode( op[c4d.DRAWHELPER_SOURCE] )
-
+    def Message(self, op, msg, data):
+        if msg == c4d.MSG_DESCRIPTION_COMMAND:
+            id = data['id'][0].id
+            if id == c4d.DRAWHELPER_OPENEDITOR:
+                self.Editor.SetDrawHelper(self)
+                self.Editor.Open(c4d.DLG_TYPE_ASYNC, self.PLUGIN_ID, 250, 200, 600, 500)
+        elif msg in (c4d.MSG_UPDATE, c4d.MSG_DESCRIPTION_POSTSETPARAMETER):
+            code = op[c4d.DRAWHELPER_SOURCE]
+            if code and (not self.func or hash(code) != self.code_hash):
+                self.func = None
+                try:
+                    scope = {'op': op, 'doc': op.GetDocument()}
+                    exec(compile(code, op.GetName(), 'exec'), scope)
+                except BaseException as exc:
+                    traceback.print_exc()
+                else:
+                    if 'main' not in scope:
+                        print("{0!r}: main function not found".format(op.GetName()))
+                    elif not callable(scope['main']):
+                        print("{0!r}: main must be callable".format(op.GetName()))
+                    else:
+                        self.func = scope['main']
+                        self.code_hash = hash(code)
+            elif not code:
+                self.func = None
         return True
 
     def Draw(self, op, drawpass, bd, bh):
-        if op.GetDeformMode() == True:
-            code    = op[c4d.DRAWHELPER_SOURCE]
-
-            globals = {
-                "raw":      self,
-                "op":       op,
-                "drawpass": drawpass,
-                "bd":       bd,
-                "bh":       bh,
-            }
-
-            exec compile(code, "<string>", "exec") in globals, globals
-
-        return True
-
-    def SendSourcecode(self, code):
-        if self.op is None:
-            return False
-
-        self.op[c4d.DRAWHELPER_SOURCE]  = code
-
+        if op.GetDeformMode() and self.func:
+            try:
+                self.func(op, drawpass, bd, bh)
+            except BaseException as exc:
+                traceback.print_exc()
         return True
 
     @classmethod
     def Register(cls):
-        icon    = utils.load_resource_bitmap('res', 'icons', 'PyDrawHelper.tif')
-
-        data    = {
-            "id":           cls.PLUGIN_ID,   # Registered !
-            "str":          "Python Draw Helper",
-            "g":            cls,
-            "description":  "Odrawhelper",
-            "info":         c4d.OBJECT_MODIFIER,
-            "icon":         icon,
-        }
-
-        RegisterObjectPlugin( **data )
+        icon = utils.load_resource_bitmap('res', 'icons', 'PyDrawHelper.tif')
+        c4d.plugins.RegisterObjectPlugin(cls.PLUGIN_ID,
+          "c4ddev Python Draw Helper", cls, "Odrawhelper",
+          c4d.OBJECT_MODIFIER, icon)
 
 DrawPassHelper.Register()
--- res/PyDrawHelper_InitialSource.py
+++ res/PyDrawHelper_InitialSource.py
@@ -1,26 +1,19 @@
-"""
-'Python Draw Helper' written by Niklas Rosenstein.
-
-Copyright by Niklas Rosenstein, 2011.
-All rights reserved.                                """
-
 import  c4d
-from    c4d     import Vector
+from c4d import Vector
 
-def main():
+def main(op, drawpass, bd, bh):
+    if drawpass != c4d.DRAWPASS_OBJECT:
+        return
     bd.SetMatrix_Matrix(op, bh.GetMg())
-
     bd.SetTransparency(1)
-    points      = (
+    points = (
         Vector(100, 0, 0),
         Vector(0, 100, 0),
         Vector(0, 0, 100),
     )
-    colors      = (
+    colors = (
         Vector(1,0,0),
         Vector(0,1,0),
         Vector(0,0,1),
     )
     bd.DrawPolygon(points, colors)
-
-main()
